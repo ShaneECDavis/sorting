71dfe49265b54a645ec59d32708f4e7c2ca0797f
Daniel Casasola <casasolad86@gmail.com>
2018-07-30 15:32:13 -0500

    switch

--- mergesort.js
+++ mergesort.js
@@ -1,4 +1,8 @@
-function mergeSort(){
+function mergeSort(arr){
+
+
+
+
 
 }
 //split function11
@@ -11,4 +15,28 @@ function split(arr){
 
 function merge(arr1,arr2){
 
+  let rightSide = 0;
+  let leftSide  = 0;
+  let tempArr = [];
+
+  let totalArrays = (arr1.length) + (arr2.length)
+
+  for(let i=0; i<totalArrays; i++){
+
+    if(arr1[leftSide] < arr2[rightSide]){
+       tempArr.push(arr1[leftSide]);
+
+
+       leftSide++
+
+     }else{
+       tempArr.push(arr2[rightSide])
+       rightSide++
+
+      //  arr2[rightSide] = arr2[rightSide] || 0
+     }
+
+  }
+ return tempArr;
 }
+
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -2,7 +2,7 @@
 
 
 describe("Merge function", function() {
-  
+
   const testArray1 = [7,6,5,4,3,2,1]
   const testArray2 = [8,7,5,4,3,2,1,6]
 
@@ -11,9 +11,15 @@ describe("Merge function", function() {
     expect(split(testArray2)).toEqual([[8,7,5,4],[3,2,1,6]])
   });
 
+  it("merges two arrays", function(){
+    expect(merge([3,4,5,8,9,10],[1,2,6,7])).toEqual([1,2,3,4,5,6,7,8,9,10])
+  });
+
 
 
 
 });
- 
+
+
+
 
